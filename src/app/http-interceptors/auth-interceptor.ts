c8f5b2aec32e194d64e5df273c807dc91e5ab310
Facundo Calbo <facuucalbo@gmail.com>
2022-06-09 14:45:09 -0300

    try 1

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable, retry, Subject, switchMap, tap } from "rxjs";
+import { BehaviorSubject, filter, Observable, retry, Subject, switchMap, take, tap, throwError } from 'rxjs';
 import { AuthResponse } from "../interfaces/api-response";
 import { AuthService } from "../services/auth.service";
 import { InventoryService } from '../services/inventory.service';
@@ -10,23 +10,23 @@ export class AuthInterceptor implements HttpInterceptor {
 
     constructor(private auth: AuthService, private inventory: InventoryService) {}
 
-    private tokenSubject = new BehaviorSubject<string>('');
+    private tokenSubject = new BehaviorSubject<any>(null);
     private isRefreshing = false;
+    private token = '';
     // asignar en cada req el access-token en el header
     // en caso de error 401 (Unauthorized) asignar el refresh token en el header
     // habria que ver como hacer para volver a mandar la request que lanzo el error
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<AuthResponse>> {
       console.log('authRed: ',req.url);
-      let token;
-      this.tokenSubject.subscribe( t => token = t)
+      this.tokenSubject.subscribe( t => this.token = t)
       // nos fijamos si la url contiene refresh-token
       const isRefresh = req.url.includes('refresh-token');
 
       isRefresh ? this.tokenSubject.next(this.auth.refreshToken) :
         this.tokenSubject.next(this.auth.accessToken);
 
-      console.log(token);
-      let authReq = this.addTokenHeader(req, token || '')
+      console.log(this.token);
+      let authReq = this.addTokenHeader(req)
 
       console.log('authReq: ', authReq);
       return next.handle(authReq)
@@ -44,18 +44,37 @@ export class AuthInterceptor implements HttpInterceptor {
             },
             error: (err) => {
               if(err.status === 401){
-                this.auth.authenticate().subscribe( t => {
-                  this.tokenSubject.next(t.accessToken);
-                })
-              } // como haces para que se vuelva a mandar la reuqest que lanzo el error
+
+                if(this.isRefreshing){
+                  return this.tokenSubject.pipe(
+                    filter(result => result !== null),
+                    take(1),
+                    switchMap(()=> next.handle(this.addTokenHeader(authReq)))
+                  )
+                }else {
+                  this.isRefreshing = true;
+                  // se setea el subject en null para q la prox api req espera a que se resetee
+                  this.tokenSubject.next(null);
+                  // se refreshea el token
+                  return this.auth.authenticate().pipe(
+                    switchMap( t => {
+                      this.tokenSubject.next(t.accessToken);
+                      return next.handle(this.addTokenHeader(authReq));
+                    })
+                  ).subscribe();
+                }
+              } else {
+                return throwError(() => err);
+              }
             }
           })
         )
     }
 
-    addTokenHeader(req: HttpRequest<any>, token: string) {
+    addTokenHeader(req: HttpRequest<any>) {
+      if(!this.token) return req;
       return req.clone({
-        setHeaders: { Authorization: token || '' }
+        setHeaders: { Authorization: this.token || '' }
       });
     }
 
